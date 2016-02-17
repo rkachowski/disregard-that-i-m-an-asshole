96237f8b824eb7e86614d92de8360e4e468d0eca
Donald Hutchison <donald.hutchison@toastymofo.net>
2016-02-17 13:52:58 +0100

    Filtering v0.0.1.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,15 +1,42 @@
+"use strict";
+
 $(function(){
     console.log("Passive aggressive fitering engaged");
 
+    var get_offenders = function(message)
+    {
+        var filter_list = ["wink"];
+        var emojis = message.find("span.emoji-outer");
+        var to_replace = [];
+        if(emojis.length > 0)
+        {
+            for(var i =0; i < emojis.length;i++)
+            {
+                var emoji = emojis[i];
+                var text = $(emoji).text();
+                for (var filtered of filter_list)
+                {
+                    var regex = new RegExp(filtered);
+                    if(text.search(regex) == 1)
+                    {
+                        to_replace.push(emoji);
+                    }
+                }
+            }
+        }
+
+        return to_replace;
+    }
+
     $("#msgs_div").bind("DOMNodeInserted", function(event){
         var inserted = $(event.target);
-        if(inserted.is("ts-message") && ! inserted.hasClass("unprocessed"))
+        if(inserted.is("ts-message") )//&& ! inserted.hasClass("unprocessed"))
         {
             var message = inserted.find(".message_body");
-            var emojis = message.find("span[class^=emoji]");
-            if(emojis.length > 0)
+            var offensive_messages = get_offenders(message);
+            if(offensive_messages.length > 0)
             {
-                console.log(emojis);
+                $(offensive_messages).replaceWith("- disregard that, i'm an asshole ");
             }
         }
     });
