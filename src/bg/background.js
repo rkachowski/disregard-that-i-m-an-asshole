ae3087d8f3c399b93490992c820455a1e67e3084
Donald Hutchison <git@toastymofo.net>
2016-02-27 14:44:24 +0100

    jesus what fuck google and declarative content + page action click listener.

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,28 +1,32 @@
 // if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
+var settings = new Store("settings", {
+    "sample_setting": "This is how you use Store.js to remember values"
+});
 
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+//
+
+
 
 // When the extension is installed or upgraded ...
 chrome.runtime.onInstalled.addListener(function() {
-  // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    // With a new rule ...
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostContains: 'slack' },
-          })
-        ],
-        // And shows the extension's page action.
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-      }
-    ]);
-  });
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        // With a new rule ...
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { hostContains: 'slack' },
+                    })
+                ],
+                // And shows the extension's page action.
+                actions: [ new chrome.declarativeContent.ShowPageAction() , hide_action]
+            }
+        ]);
+    });
 });
+
